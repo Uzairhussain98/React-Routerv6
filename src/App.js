c319d58eb91dab0155d9c3447d8252d91da0fdb5
Uzair Hussain <uzairhussain98@hotmail.com>
2020-08-11 16:02:34 +0500

    started routerv6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+
+       <navbar>
+        <Link to="/">Home</Link>
+        <Link to="/Store">Store</Link> 
+      </navbar> 
+
+
+      <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="Store" element={<Store/>} >
+      <Route path="/" element={<LaunchIndex/>}/>
+
+</Route>
+   </Routes>
+  </Router>
+    
+  
+  )};
+
+
+    function Home(){
+      return(
+      <div>
+        <h1>Hello From Home</h1>
+        </div>
+
+
+      )}
+
+
+
+    function Store(){
+      return(
+      <div>
+        <h1>Hello From Store</h1>
+        <Outlet/>
+        </div>
+
+
+      )}
+
+      function LaunchIndex(){
+        return(
+          <ul>
+            {Object.entries(shoes).map(([slug , {name ,img }]) => 
+            <li key={slug}>
+              <h2>{name}</h2>
+              <img src={img} alt={name}/>
+
+            </li> )}
+
+
+
+
+
+            </ul>
+
+
+        )}
+
+
+
+
+
+
+
+
+
+  const shoes = {
+    "air-jordan-3-valor-blue": {
+      name: "VALOUR BLUE",
+      img:
+        "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+    },
+    "jordan-mars-270-london": {
+      name: "JORDAN MARS 270 LONDON",
+      img:
+        "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+    },
+    "air-jordan-1-zoom-racer-blue": {
+      name: "RACER BLUE",
+      img:
+        "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+    }
+  };
+
 
 export default App;
